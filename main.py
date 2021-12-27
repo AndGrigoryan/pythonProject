f9416c8690eb285d5f5936c945f28d81d6850807
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2021-12-27 21:18:45 +0400

    Working with files

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-def my_print(*params, endl="\n"):
-    print(*params, end=endl)
+import os
 
+path = "test"
 
-my_print(2, 54, 4)
-my_print(34, 4, endl="\t")
-my_print("Hello", endl=" ")
-my_print("John")
-my_print(27)
+if not os.path.exists(path):
+    os.mkdir(path)
+    print(f"{path} directory created successfully")
+else:
+    print(f"Failed to create directory {path}")
+
+os.chdir(path)
+
+with open("data.txt", "a", encoding="utf-8") as f:
+    f.write("Test ")
